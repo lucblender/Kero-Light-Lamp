662cb79baa1f43063928061e5dd2826122033bfe
lucblender <lucasbonvin@hotmail.com>
2022-03-27 20:28:00 +0200

    pass from 3 to one mode with more randomness

--- main.py
+++ main.py
@@ -5,7 +5,11 @@ from FireNeopixel import *
 MIST_ON = 0
 MIST_OFF = 1
 
-mode = 0
+mode = False
+fan_on_counter = 0
+update_mist_counter = 0
+up_to = 0
+old_up_to = 0
 
 led = Pin(25, Pin.OUT)
 fan = Pin(22, Pin.OUT)
@@ -29,11 +33,20 @@ def debounce(pin):
 def button_listener(pin):
     print("button_listener")    
     global mode
-    mode = (mode+1)%mode_number
-    turn_on_up_to(mode)
+    mode = not mode
     
 button.irq(debounce, Pin.IRQ_FALLING)
 
+def update_mist_strength():
+    global update_mist_counter, up_to, old_up_to
+    if update_mist_counter%35 == 0:
+        up_to = random.randrange(1,4)
+        if old_up_to != up_to:
+            turn_off_all()
+            turn_on_up_to(up_to)            
+            print(up_to)
+        old_up_to = up_to
+    update_mist_counter += 1
 
 def turn_on_up_to(index):
     if index == 0:        
@@ -67,26 +80,48 @@ def blink_led():
     time.sleep(0.05)
     led.value(0)
     time.sleep(0.05)
-
-def random_fan_lifecycle():
-    if random.random()>0.95:
-        fan.value(not fan.value())
+    
 def fan_clear():
     fan.value(0)
+    
+def fan_on():
+    fan.value(1)
+
+def random_fan_lifecycle():
+    global fan_on_counter
+    if fan.value() == 1:
+        fan_on_counter += 1
+        if fan_on_counter > 40:
+            print("ici0")
+            fan_clear()
+            fan_on_counter = 0
+        elif fan_on_counter > 10:   
+            if random.random()>0.95:  
+                print("ici1")              
+                fan_clear()
+                fan_on_counter = 0
+    else:
+        if random.random()>0.95:
+            fan_on()
+        
+
     
 turn_off_all()
 clear()
 
 while True:
-    if mode != 0:
+    if mode == True:
         draw()
-        random_fan_lifecycle()  
+        random_fan_lifecycle()
+        update_mist_strength()
     else:
+        update_mist_counter = 0
+        up_to = 0
+        old_up_to = 0
         clear()
         fan_clear()
-        
-         
-        
+        turn_off_all()
+
     time.sleep_ms(random.randrange(50,150))
 
     
